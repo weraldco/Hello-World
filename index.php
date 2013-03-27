fc529ebf5c49fe21b85e1cfba65fda1c694ffc9c
Werald Opolento <werald.opolento@gmail.com>
2013-03-27 09:47:02 +0800

    Add css folder and file and update index file

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GitHub Testing</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		
+		<?php 
 
-	
-	echo "Hello World";
+			echo "Hello World";
 
-	echo "The world is full excitement.";
+			echo "The world is full excitement.";
 
-	echo "Hey <a href='message.php'>click me!</a>";
+			echo "Hey <a href='message.php'>click me!</a>";
 
-?>
+		?>
+	</body>
+</html>
